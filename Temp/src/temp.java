2e03773a0e9bc69e774e44780a30b1b8be3bf0af
1514968742 <1514968742@qq.com>
2016-08-31 11:18:33 -0700

    flex
    11

---
+++ Temp/src/temp.java
@@ -0,0 +1,11 @@
+
+public class temp {
+
+	public static void main(String[] args) {
+		double Fahrenheit=333;
+		double celsius;
+		celsius =(5.0/9.0)*(Fahrenheit-32);
+		System.out.println(celsius);
+	}
+
+}
