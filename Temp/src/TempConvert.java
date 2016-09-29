b855cf48ce87e015db3771068c650b45e3c6b190
1514968742 <1514968742@qq.com>
2016-09-29 09:53:14 -0700

    111

---
+++ Temp/src/TempConvert.java
@@ -0,0 +1,11 @@
+
+public class TempConvert{
+
+	public static void main(String[] args) {
+		double Fahrenheit=99;
+		double celsius;
+		celsius =(5.0/9.0)*(Fahrenheit-32);
+		System.out.println(celsius);
+	}
+
+}
